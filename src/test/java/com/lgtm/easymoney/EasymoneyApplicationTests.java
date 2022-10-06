ba06791cb5401493dce3ce46b9eecaee22bc6a5f
H Lin <34226262+howieraem@users.noreply.github.com>
2022-10-06 14:33:07 -0400

    [add] spring initializr files

---
+++ src/test/java/com/lgtm/easymoney/EasymoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgtm.easymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
