29179079584629dcbf77df19f5b6fefc812dd8ed
H Lin <34226262+howieraem@users.noreply.github.com>
2022-10-15 19:30:51 -0400

    Update UserControllerTest.java

--- src/test/java/com/lgtm/easymoney/controllers/UserControllerTest.java
+++ src/test/java/com/lgtm/easymoney/controllers/UserControllerTest.java
@@ -1,5 +1,7 @@
 package com.lgtm.easymoney.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lgtm.easymoney.payload.BalanceReq;
 import com.lgtm.easymoney.payload.BalanceRsp;
 import com.lgtm.easymoney.services.UserService;
@@ -8,9 +10,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -22,12 +28,40 @@ public class UserControllerTest {
     private UserService userService;
 
     @Test
-    public void deposit() {
+    public void depositSuccess() {
 
     }
 
     @Test
-    public void withdraw() {
+    public void depositFailedByInvalidAmount() {
 
+    }
+
+    @Test
+    public void withdrawSuccess() {
+
+    }
+
+    @Test
+    public void withdrawFailedByInvalidAmount() {
+
+    }
+
+    public ResultActions postDeposit(BalanceReq req) throws Exception {
+        return mvc.perform(post("/user/deposit")
+                .content(asJsonString(req))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+    }
+
+    public ResultActions postWithdraw(BalanceReq req) throws Exception {
+        return mvc.perform(post("/user/withdraw")
+                .content(asJsonString(req))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+    }
+
+    public static String asJsonString(final Object obj) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(obj);
     }
 }
