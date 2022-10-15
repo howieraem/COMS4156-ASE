015f4206b908121d42ccb9e52ebe12c3b342420a
H Lin <34226262+howieraem@users.noreply.github.com>
2022-10-15 18:35:28 -0400

    [add] some mocking and integration tests

--- src/test/java/com/lgtm/easymoney/services/UserServiceImplTest.java
+++ src/test/java/com/lgtm/easymoney/services/UserServiceImplTest.java
@@ -1,17 +1,22 @@
 package com.lgtm.easymoney.services;
 
 import com.lgtm.easymoney.exceptions.ResourceNotFoundException;
+import com.lgtm.easymoney.models.Account;
 import com.lgtm.easymoney.models.User;
+import com.lgtm.easymoney.payload.BalanceReq;
 import com.lgtm.easymoney.repositories.UserRepository;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.BDDMockito;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -35,14 +40,16 @@ public class UserServiceImplTest {
         user2.setId(2L);
         user2.setEmail("b@b.com");
         user2.setPassword("b");
+        user2.setBalance(new BigDecimal(100));
 
-        Mockito.when(userRepository
-                .findById(user1.getId())).thenReturn(Optional.of(user1));
+        Mockito.when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
+        Mockito.when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
+        Mockito.when(userRepository.existsByEmail(user1.getEmail())).thenReturn(Boolean.TRUE);
+        Mockito.when(userRepository.existsByEmail(user2.getEmail())).thenReturn(Boolean.FALSE);
 
         List<User> users = new ArrayList<>();
         users.add(user1);
         users.add(user2);
-
         Mockito.when(userRepository.findAll()).thenReturn(users);
     }
 
@@ -50,20 +57,78 @@ public class UserServiceImplTest {
     public void shouldGetUserByID() {
         Long id = 1L;
         User user = userService.getUserByID(id);
-        Assert.assertEquals(user.getId(), id);
+        assertEquals(user.getId(), id);
     }
 
     @Test
     public void shouldThrowExceptionWhenUserNotFound() {
         Long id = 3L;
-        Assert.assertThrows(ResourceNotFoundException.class, () -> {
+        Mockito.when(userRepository.findById(id)).thenReturn(Optional.empty());
+        assertThrows(ResourceNotFoundException.class, () -> {
             userService.getUserByID(id);
         });
+    }
+
+    // Perhaps this is better done in the integration test?
+    @Test
+    public void shouldSaveUser() {
+        User user = new User();
+        user.setId(3L);
+        user.setEmail("c@c.com");
+        user.setPassword("c");
+
+        // Assume userRepository.save() always succeeds
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        User saved = userService.saveUser(user);
+
+        assertNotNull(saved);
+        assertEquals(saved.getId(), user.getId());
+        assertEquals(saved.getEmail(), user.getEmail());
+        assertEquals(saved.getPassword(), user.getPassword());
     }
 
     @Test
     public void shouldGetAllUsers() {
         List<User> users = userService.getAllUsers();
-        Assert.assertEquals(users.size(), 2);
+        assertEquals(users.size(), 2);
+    }
+
+    @Test
+    public void shouldDepositSuccessfully() {
+        BalanceReq req = new BalanceReq();
+        req.setUid(1L);
+        req.setAmount(new BigDecimal(100));
+        var rsp = userService.makeADeposit(req);
+        var body = rsp.getBody();
+        assertEquals(rsp.getStatusCode(), HttpStatus.OK);
+        assertNotNull(body);
+        assertTrue(body.getSuccess());
+        assertEquals(body.getCurrBalance(), req.getAmount());
+    }
+
+    @Test
+    public void shouldWithdrawSuccessfully() {
+        BalanceReq req = new BalanceReq();
+        req.setUid(2L);
+        req.setAmount(new BigDecimal(100));
+        var rsp = userService.makeAWithdraw(req);
+        var body = rsp.getBody();
+        assertEquals(rsp.getStatusCode(), HttpStatus.OK);
+        assertNotNull(body);
+        assertTrue(body.getSuccess());
+        assertEquals(body.getCurrBalance(), BigDecimal.ZERO);
+    }
+
+    @Test
+    public void shouldNotWithdrawIfAmountExceedsBalance() {
+        BalanceReq req = new BalanceReq();
+        req.setUid(1L);
+        req.setAmount(new BigDecimal(100));
+        var rsp = userService.makeAWithdraw(req);
+        var body = rsp.getBody();
+        assertEquals(rsp.getStatusCode(), HttpStatus.BAD_REQUEST);
+        assertNotNull(body);
+        assertFalse(body.getSuccess());
+        assertEquals(body.getCurrBalance(), BigDecimal.ZERO);
     }
 }
