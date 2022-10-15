e120c829447525107112c56ee96b71a686a70c86
Ruize Li <rl3250@columbia.edu>
2022-10-14 21:15:03 -0400

    changed mapping route

--- src/main/java/com/lgtm/easymoney/controllers/UserController.java
+++ src/main/java/com/lgtm/easymoney/controllers/UserController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/balance")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
