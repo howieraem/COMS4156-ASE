29f2a6fcfc4ec001e09a3078c84994012bf5bd9b
H Lin <34226262+howieraem@users.noreply.github.com>
2022-10-15 19:35:32 -0400

    removed unused code

--- src/main/java/com/lgtm/easymoney/repositories/UserRepository.java
+++ src/main/java/com/lgtm/easymoney/repositories/UserRepository.java
@@ -4,10 +4,7 @@ import com.lgtm.easymoney.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
-    User findByEmail(String email);
 }
