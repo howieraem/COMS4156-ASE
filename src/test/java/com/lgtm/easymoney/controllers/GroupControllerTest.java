07fc93145ee5cafeb3689880d26d88a4c48d02b3
Ruize Li <rl3250@columbia.edu>
2022-10-20 10:05:29 -0400

    comment out uninitialized test before merge back to main

--- src/test/java/com/lgtm/easymoney/controllers/GroupControllerTest.java
+++ src/test/java/com/lgtm/easymoney/controllers/GroupControllerTest.java
@@ -1,20 +1,20 @@
-package com.lgtm.easymoney.controllers;
-
-import com.lgtm.easymoney.services.GroupService;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(GroupController.class)
-public class GroupControllerTest {
-    @Autowired
-    private MockMvc mvc;
-
-    @MockBean
-    private GroupService groupService;
-
-}
+//package com.lgtm.easymoney.controllers;
+//
+//import com.lgtm.easymoney.services.GroupService;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(GroupController.class)
+//public class GroupControllerTest {
+//    @Autowired
+//    private MockMvc mvc;
+//
+//    @MockBean
+//    private GroupService groupService;
+//
+//}
