98aee30d7c91f94ba52feacc3cb80cf43d8e05a1
H Lin <34226262+howieraem@users.noreply.github.com>
2022-10-24 20:15:52 -0400

    [fix] code style in tests

--- src/test/java/com/lgtm/easymoney/EasymoneyApplicationTests.java
+++ src/test/java/com/lgtm/easymoney/EasymoneyApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EasymoneyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
