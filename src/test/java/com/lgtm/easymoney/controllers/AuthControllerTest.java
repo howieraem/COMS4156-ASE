f1a312e43bb87cdf8d2a24552c4064069c79cd9d
Ken Xiong <kx2175@columbia.edu>
2022-10-24 20:25:39 -0400

    fix style on testcases

--- src/test/java/com/lgtm/easymoney/controllers/AuthControllerTest.java
+++ src/test/java/com/lgtm/easymoney/controllers/AuthControllerTest.java
@@ -1,5 +1,9 @@
 package com.lgtm.easymoney.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lgtm.easymoney.models.User;
@@ -17,188 +21,186 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AuthController.class)
 public class AuthControllerTest {
-    @Autowired
-    private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-    @MockBean
-    private UserService userService;
+  @MockBean
+  private UserService userService;
 
-    private static RegisterReq req;
+  private static RegisterReq req;
 
-    @Before
-    public void setup() {
-        req = new RegisterReq();
-        req.setEmail("a@a.com");
-        req.setPassword("a");
-        req.setUserType("PERSONAL");
-        req.setAccountName("a");
-        req.setAccountNumber("123");
-        req.setRoutingNumber("123456789");
+  @Before
+  public void setup() {
+    req = new RegisterReq();
+    req.setEmail("a@a.com");
+    req.setPassword("a");
+    req.setUserType("PERSONAL");
+    req.setAccountName("a");
+    req.setAccountNumber("123");
+    req.setRoutingNumber("123456789");
 
-        // Assume userService.saveUser() always succeeds
-        Mockito.when(userService.saveUser(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
-    }
+    // Assume userService.saveUser() always succeeds
+    Mockito.when(userService.saveUser(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+  }
 
-    @Test
-    public void registerSuccessful() throws Exception {
-        postRegister(req).andExpect(status().isCreated()); // when field values are all valid
-    }
+  @Test
+  public void registerSuccessful() throws Exception {
+    postRegister(req).andExpect(status().isCreated()); // when field values are all valid
+  }
 
-    @Test
-    public void registerFailedByInvalidEmail() throws Exception {
-        // email is null
-        req.setEmail(null);
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("email"));
+  @Test
+  public void registerFailedByInvalidEmail() throws Exception {
+    // email is null
+    req.setEmail(null);
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("email"));
 
-        // email is empty
-        req.setEmail("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("email"));
+    // email is empty
+    req.setEmail("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("email"));
 
-        // email doesn't contain '@'
-        req.setEmail("a");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("email"));
+    // email doesn't contain '@'
+    req.setEmail("a");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("email"));
 
-        // email contains more than 1 '@'
-        req.setEmail("a@b.com@.com");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("email"));
-    }
+    // email contains more than 1 '@'
+    req.setEmail("a@b.com@.com");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("email"));
+  }
 
-    @Test
-    public void registerFailedByInvalidPassword() throws Exception {
-        // password is null
-        req.setPassword(null);
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("password"));
+  @Test
+  public void registerFailedByInvalidPassword() throws Exception {
+    // password is null
+    req.setPassword(null);
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("password"));
 
-        // password is empty
-        req.setPassword("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("password"));
-    }
+    // password is empty
+    req.setPassword("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("password"));
+  }
 
-    @Test
-    public void registerFailedByInvalidUserType() throws Exception {
-        // user type is null
-        req.setUserType(null);
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("userType"));
+  @Test
+  public void registerFailedByInvalidUserType() throws Exception {
+    // user type is null
+    req.setUserType(null);
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("userType"));
 
-        // user type is empty
-        req.setUserType("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("userType"));
+    // user type is empty
+    req.setUserType("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("userType"));
 
-        // user type is not in enum
-        req.setUserType("abc");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("userType"));
-    }
+    // user type is not in enum
+    req.setUserType("abc");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("userType"));
+  }
 
-    @Test
-    public void registerFailedByInvalidPhone() throws Exception {
-        // phone is not null but empty
-        req.setPhone("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("phone"));
+  @Test
+  public void registerFailedByInvalidPhone() throws Exception {
+    // phone is not null but empty
+    req.setPhone("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("phone"));
 
-        // phone contains non-numeric characters
-        req.setPhone("abc");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("phone"));
+    // phone contains non-numeric characters
+    req.setPhone("abc");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("phone"));
 
-        // phone is too short
-        req.setPhone("000");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("phone"));
+    // phone is too short
+    req.setPhone("000");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("phone"));
 
-        // phone is too long
-        req.setPhone("00000000000");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("phone"));
-    }
+    // phone is too long
+    req.setPhone("00000000000");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("phone"));
+  }
 
-    @Test
-    public void registerFailedByInvalidAccountName() throws Exception {
-        // account name is null
-        req.setAccountName(null);
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("accountName"));
+  @Test
+  public void registerFailedByInvalidAccountName() throws Exception {
+    // account name is null
+    req.setAccountName(null);
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("accountName"));
 
-        // account name is empty
-        req.setAccountName("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("accountName"));
-    }
+    // account name is empty
+    req.setAccountName("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("accountName"));
+  }
 
-    @Test
-    public void registerFailedByInvalidAccountNumber() throws Exception {
-        // account number is null
-        req.setAccountNumber(null);
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("accountNumber"));
+  @Test
+  public void registerFailedByInvalidAccountNumber() throws Exception {
+    // account number is null
+    req.setAccountNumber(null);
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("accountNumber"));
 
-        // account number is empty
-        req.setAccountNumber("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("accountNumber"));
+    // account number is empty
+    req.setAccountNumber("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("accountNumber"));
 
-        // account number contains non-numeric characters
-        req.setAccountNumber("abc");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("accountNumber"));
+    // account number contains non-numeric characters
+    req.setAccountNumber("abc");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("accountNumber"));
 
-        // account number is too long
-        req.setAccountNumber("12321371892473218947122");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("accountNumber"));
-    }
+    // account number is too long
+    req.setAccountNumber("12321371892473218947122");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("accountNumber"));
+  }
 
-    @Test
-    public void registerFailedByInvalidRoutingNumber() throws Exception {
-        // routing number is null
-        req.setRoutingNumber(null);
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("routingNumber"));
+  @Test
+  public void registerFailedByInvalidRoutingNumber() throws Exception {
+    // routing number is null
+    req.setRoutingNumber(null);
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("routingNumber"));
 
-        // routing number is empty
-        req.setRoutingNumber("");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("routingNumber"));
+    // routing number is empty
+    req.setRoutingNumber("");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("routingNumber"));
 
-        // routing number contains non-numeric characters
-        req.setRoutingNumber("abc");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("routingNumber"));
+    // routing number contains non-numeric characters
+    req.setRoutingNumber("abc");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("routingNumber"));
 
-        // routing number is too short
-        req.setRoutingNumber("12321");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("routingNumber"));
+    // routing number is too short
+    req.setRoutingNumber("12321");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("routingNumber"));
 
-        // routing number is too long
-        req.setRoutingNumber("12321371892473218947122");
-        postRegister(req).andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorFields").value("routingNumber"));
-    }
+    // routing number is too long
+    req.setRoutingNumber("12321371892473218947122");
+    postRegister(req).andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.errorFields").value("routingNumber"));
+  }
 
-    public ResultActions postRegister(RegisterReq req) throws Exception {
-        return mvc.perform(post("/user/register")
-                .content(asJsonString(req))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON));
-    }
+  public ResultActions postRegister(RegisterReq req) throws Exception {
+    return mvc.perform(post("/user/register")
+            .content(asJsonString(req))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON));
+  }
 
-    public static String asJsonString(final Object obj) throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(obj);
-    }
+  public static String asJsonString(final Object obj) throws JsonProcessingException {
+    return new ObjectMapper().writeValueAsString(obj);
+  }
 }
--- src/test/java/com/lgtm/easymoney/services/impl/SearchServiceImplTest.java
+++ src/test/java/com/lgtm/easymoney/services/impl/SearchServiceImplTest.java
@@ -1,5 +1,9 @@
 package com.lgtm.easymoney.services.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+
 import com.lgtm.easymoney.exceptions.ResourceNotFoundException;
 import com.lgtm.easymoney.models.Account;
 import com.lgtm.easymoney.models.User;
@@ -7,6 +11,9 @@ import com.lgtm.easymoney.payload.ProfilesRsp;
 import com.lgtm.easymoney.repositories.AccountRepository;
 import com.lgtm.easymoney.repositories.UserRepository;
 import com.lgtm.easymoney.services.UserService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,186 +22,194 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-
+/**
+ * Search Service Test.
+ */
 @RunWith(SpringRunner.class)
 public class SearchServiceImplTest {
-    @InjectMocks
-    private SearchServiceImpl searchService;
+  @InjectMocks
+  private SearchServiceImpl searchService;
 
-    @Mock
-    private UserService userService;
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private AccountRepository accountRepository;
+  @Mock
+  private UserService userService;
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private AccountRepository accountRepository;
 
 
-    private User user1;
-    private User user2;
-    private Long id1 = 1L;
-    private Long id2 = 2L;
-    private Account account1;
-    private Account account2;
+  private User user1;
+  private User user2;
+  private Long id1 = 1L;
+  private Long id2 = 2L;
+  private Account account1;
+  private Account account2;
 
-    @Before
-    public void setUp() {
-        user1 = new User();
-        account1 = new Account();
-        user1.setId(id1);
-        user1.setEmail("a@a.com");
-        user1.setPhone("1234562211");
-        user1.setPassword("a");
-        account1.setId(id1);
-        account1.setAccountUser(user1);
-        account1.setAccountName("abc");
-        user1.setAccount(account1);
+  /**
+   * Setup.
+   */
+  @Before
+  public void setUp() {
+    user1 = new User();
+    account1 = new Account();
+    user1.setId(id1);
+    user1.setEmail("a@a.com");
+    user1.setPhone("1234562211");
+    user1.setPassword("a");
+    account1.setId(id1);
+    account1.setAccountUser(user1);
+    account1.setAccountName("abc");
+    user1.setAccount(account1);
 
-        user2 = new User();
-        account2 = new Account();
-        user2.setId(id2);
-        user2.setEmail("b@b.com");
-        user1.setPhone("1234562210");
-        user2.setPassword("b");
-        account2.setId(id2);
-        account2.setAccountUser(user2);
-        account2.setAccountName("acd");
-        user2.setAccount(account2);
+    user2 = new User();
+    account2 = new Account();
+    user2.setId(id2);
+    user2.setEmail("b@b.com");
+    user1.setPhone("1234562210");
+    user2.setPassword("b");
+    account2.setId(id2);
+    account2.setAccountUser(user2);
+    account2.setAccountName("acd");
+    user2.setAccount(account2);
 
-        Mockito.when(userService.getUserById(id1)).thenReturn(user1);
-        Mockito.when(userService.getUserById(id2)).thenReturn(user2);
+    Mockito.when(userService.getUserById(id1)).thenReturn(user1);
+    Mockito.when(userService.getUserById(id2)).thenReturn(user2);
 
-        Mockito.when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
-        Mockito.when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
-        Mockito.when(userRepository.existsById(user1.getId())).thenReturn(true);
-        Mockito.when(userRepository.existsById(user2.getId())).thenReturn(true);
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        Mockito.when(userRepository.findAll()).thenReturn(users);
+    Mockito.when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
+    Mockito.when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
+    Mockito.when(userRepository.existsById(user1.getId())).thenReturn(true);
+    Mockito.when(userRepository.existsById(user2.getId())).thenReturn(true);
+    List<User> users = new ArrayList<>();
+    users.add(user1);
+    users.add(user2);
+    Mockito.when(userRepository.findAll()).thenReturn(users);
 
-        Mockito.when(accountRepository.findById(id1)).thenReturn(Optional.of(account1));
-        Mockito.when(accountRepository.findById(id2)).thenReturn(Optional.of(account2));
-    }
+    Mockito.when(accountRepository.findById(id1)).thenReturn(Optional.of(account1));
+    Mockito.when(accountRepository.findById(id2)).thenReturn(Optional.of(account2));
+  }
 
-    @Test
-    public void getUserByIdSuccess() {
-        // Act
-        User user = searchService.getUserById(id1);
+  @Test
+  public void getUserByIdSuccess() {
+    // Act
+    User user = searchService.getUserById(id1);
 
-        // Assert
-        assertEquals(user1, user);
-    }
+    // Assert
+    assertEquals(user1, user);
+  }
 
-    @Test
-    public void getUserByNameSuccess() {
-        // Arrange
-        Mockito.when(accountRepository.findByAccountNameContainingIgnoreCase(account1.getAccountName())).thenReturn(List.of(account1));
+  @Test
+  public void getUserByNameSuccess() {
+    // Arrange
+    Mockito.when(accountRepository.findByAccountNameContainingIgnoreCase(
+            account1.getAccountName())).thenReturn(List.of(account1));
 
-        // Act
-        User user = searchService.getUserByName(account1.getAccountName()).get(0);
+    // Act
+    User user = searchService.getUserByName(account1.getAccountName()).get(0);
 
-        // Assert
-        assertEquals(user1,user);
-    }
+    // Assert
+    assertEquals(user1, user);
+  }
 
-    @Test
-    public void getUserByNameSuccessWithMultipleMatch() {
-        // Arrange
-        String searchStr = "a";
-        Mockito.when(accountRepository.findByAccountNameContainingIgnoreCase(searchStr)).thenReturn(List.of(account1,account2));
+  @Test
+  public void getUserByNameSuccessWithMultipleMatch() {
+    // Arrange
+    String searchStr = "a";
+    Mockito.when(accountRepository.findByAccountNameContainingIgnoreCase(
+            searchStr)).thenReturn(List.of(account1, account2));
 
-        // Act
-        List<User> userList = searchService.getUserByName("a");
+    // Act
+    List<User> userList = searchService.getUserByName("a");
 
-        // Assert
-        assertEquals(2,userList.size());
-    }
+    // Assert
+    assertEquals(2, userList.size());
+  }
 
-    @Test
-    public void getUserByEmailOrPhoneSuccess() {
-        // Arrange
-        Mockito.when(userRepository.findByEmailContainingIgnoreCaseOrPhoneContaining(any(),any())).thenReturn(List.of(user1));
+  @Test
+  public void getUserByEmailOrPhoneSuccess() {
+    // Arrange
+    Mockito.when(userRepository.findByEmailContainingIgnoreCaseOrPhoneContaining(
+            any(), any())).thenReturn(List.of(user1));
 
-        // Act
-        User user = searchService.getUserByEmailOrPhone(user1.getEmail(), user1.getPhone()).get(0);
+    // Act
+    User user = searchService.getUserByEmailOrPhone(user1.getEmail(), user1.getPhone()).get(0);
 
-        // Assert
-        assertEquals(user1,user);
-    }
+    // Assert
+    assertEquals(user1, user);
+  }
 
-    @Test
-    public void getUserByIdFailedWithNonExistingId() {
-        // Act && Assert
-        assertThrows(ResourceNotFoundException.class, () -> searchService.getUserById(3L));
-    }
+  @Test
+  public void getUserByIdFailedWithNonExistingId() {
+    // Act && Assert
+    assertThrows(ResourceNotFoundException.class, () -> searchService.getUserById(3L));
+  }
 
-    @Test
-    public void getUserByNameFailedWithNonExistingName() {
-        // Arrange
-        Account fakeAccount = new Account();
-        fakeAccount.setAccountName("123");
+  @Test
+  public void getUserByNameFailedWithNonExistingName() {
+    // Arrange
+    Account fakeAccount = new Account();
+    fakeAccount.setAccountName("123");
 
-        // Act
-        List<User> userList = searchService.getUserByName(fakeAccount.getAccountName());
+    // Act
+    List<User> userList = searchService.getUserByName(fakeAccount.getAccountName());
 
-        // Assert
-        assertEquals(0,userList.size());
-    }
+    // Assert
+    assertEquals(0, userList.size());
+  }
 
-    @Test
-    public void getUserByEmailOrPhoneFailedWithNonExistingEmailOrPhone() {
-        // Arrange
-        User user = new User();
-        user.setEmail("foo@foo.com");
+  @Test
+  public void getUserByEmailOrPhoneFailedWithNonExistingEmailOrPhone() {
+    // Arrange
+    User user = new User();
+    user.setEmail("foo@foo.com");
 
-        // Act
-        List<User> returnedUserList = searchService.getUserByEmailOrPhone(user.getEmail(), user.getPhone());
+    // Act
+    List<User> returnedUserList = searchService.getUserByEmailOrPhone(
+            user.getEmail(), user.getPhone());
 
-        // Assert
-        assertEquals(0,returnedUserList.size());
-    }
+    // Assert
+    assertEquals(0, returnedUserList.size());
+  }
 
-    @Test
-    public void searchByIdSuccess() {
-        ProfilesRsp rsp = searchService.searchById(id1);
-        assertEquals(id1, rsp.getUserProfiles().get(0).getUid());
-        assertEquals(user1.getEmail(), rsp.getUserProfiles().get(0).getEmail());
-        assertEquals(user1.getAccount().getAccountName(), rsp.getUserProfiles().get(0).getAccountName());
-    }
+  @Test
+  public void searchByIdSuccess() {
+    ProfilesRsp rsp = searchService.searchById(id1);
+    assertEquals(id1, rsp.getUserProfiles().get(0).getUid());
+    assertEquals(user1.getEmail(),
+            rsp.getUserProfiles().get(0).getEmail());
+    assertEquals(user1.getAccount().getAccountName(),
+            rsp.getUserProfiles().get(0).getAccountName());
+  }
 
-    @Test
-    public void searchByInfoSuccess() {
-        List<User> userList = new ArrayList<>();
-        userList.add(user1);
-        Mockito.when(userRepository.findByEmailContainingIgnoreCaseOrPhoneContaining(
-                user1.getEmail(), user1.getEmail())).thenReturn(userList);
-        ProfilesRsp rsp = searchService.searchByInfo(user1.getEmail());
-        assertEquals(id1, rsp.getUserProfiles().get(0).getUid());
-        assertEquals(user1.getEmail(), rsp.getUserProfiles().get(0).getEmail());
-        assertEquals(user1.getAccount().getAccountName(), rsp.getUserProfiles().get(0).getAccountName());
-        assertEquals(user1.getPhone(), rsp.getUserProfiles().get(0).getPhone());
-    }
+  @Test
+  public void searchByInfoSuccess() {
+    List<User> userList = new ArrayList<>();
+    userList.add(user1);
+    Mockito.when(userRepository.findByEmailContainingIgnoreCaseOrPhoneContaining(
+            user1.getEmail(), user1.getEmail())).thenReturn(userList);
+    ProfilesRsp rsp = searchService.searchByInfo(user1.getEmail());
+    assertEquals(id1,
+            rsp.getUserProfiles().get(0).getUid());
+    assertEquals(user1.getEmail(),
+            rsp.getUserProfiles().get(0).getEmail());
+    assertEquals(user1.getAccount().getAccountName(),
+            rsp.getUserProfiles().get(0).getAccountName());
+    assertEquals(user1.getPhone(),
+            rsp.getUserProfiles().get(0).getPhone());
+  }
 
-    @Test
-    public void searchByInfoSuccessMultipleMatch() {
-        List<User> userList = new ArrayList<>();
-        userList.add(user1);
-        userList.add(user2);
-        String strString = "a";
-        Mockito.when(userRepository.findByEmailContainingIgnoreCaseOrPhoneContaining(
-                strString, strString)).thenReturn(userList);
-        Mockito.when(accountRepository.findByAccountNameContainingIgnoreCase(
-                strString)).thenReturn(List.of(account1,account2));
+  @Test
+  public void searchByInfoSuccessMultipleMatch() {
+    List<User> userList = new ArrayList<>();
+    userList.add(user1);
+    userList.add(user2);
+    String strString = "a";
+    Mockito.when(userRepository.findByEmailContainingIgnoreCaseOrPhoneContaining(
+            strString, strString)).thenReturn(userList);
+    Mockito.when(accountRepository.findByAccountNameContainingIgnoreCase(
+            strString)).thenReturn(List.of(account1, account2));
 
-        ProfilesRsp rsp = searchService.searchByInfo("a");
-        assertEquals(2, rsp.getUserProfiles().size());
-    }
+    ProfilesRsp rsp = searchService.searchByInfo("a");
+    assertEquals(2, rsp.getUserProfiles().size());
+  }
 
 }
