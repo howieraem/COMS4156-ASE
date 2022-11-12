c7c3d7369aef1883fbd69db79059728f066d008c
H Lin <34226262+howieraem@users.noreply.github.com>
2022-11-12 18:35:09 -0500

    [refactor] loan service

--- src/main/java/com/lgtm/easymoney/services/LoanService.java
+++ src/main/java/com/lgtm/easymoney/services/LoanService.java
@@ -1,5 +1,6 @@
 package com.lgtm.easymoney.services;
 
+import com.lgtm.easymoney.models.User;
 import com.lgtm.easymoney.payload.req.RequestAcceptDeclineReq;
 import com.lgtm.easymoney.payload.req.RequestReq;
 import com.lgtm.easymoney.payload.rsp.LoanRsp;
@@ -9,11 +10,11 @@ import com.lgtm.easymoney.payload.rsp.ResourceCreatedRsp;
  * Loan service interface.
  */
 public interface LoanService {
-  ResourceCreatedRsp requestLoan(RequestReq req);
+  ResourceCreatedRsp requestLoan(User borrower, RequestReq req);
 
-  LoanRsp getLoansByUid(Long uid);
+  LoanRsp getLoansByUser(User user);
 
-  LoanRsp approveLoan(RequestAcceptDeclineReq req);
+  LoanRsp approveLoan(User lender, RequestAcceptDeclineReq req);
 
-  LoanRsp declineLoan(RequestAcceptDeclineReq req);
+  LoanRsp declineLoan(User lender, RequestAcceptDeclineReq req);
 }
